4ad91a96abf00106d0f725920cbc7a34471bc153
TTTPPPPP <906483498@qq.com>
2024-05-31 00:12:53 +0800

    add -std=c++20 compile option

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build>nul 2>nul
-g++ -Wall -static  .\AutoTask.cpp -o .\build\AutoTask.exe -mwindows
+g++ -Wall -static  .\AutoTask.cpp -o .\build\AutoTask.exe -mwindows -std=c++20
 strip .\build\AutoTask.exe
 echo.finished!
 pause>nul
