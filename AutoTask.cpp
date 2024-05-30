6e2b231df1aba91a35ba30f2ceb5259a8884b2fa
TTTPPPPP <906483498@qq.com>
2024-05-31 00:13:57 +0800

    detach timeval getting and main process

--- AutoTask.cpp
+++ AutoTask.cpp
@@ -1,65 +1,92 @@
 #include <thread>
+#include <algorithm>
 #include "includes\winruntime.hpp"
 #include "includes\Ptime.hpp"
 #include "includes\DataGroup.hpp"
 #include "includes\Pconf.hpp"
 #include "includes\PIO.hpp"
 
-const bool DEBUG = true;
-Rule GetCurRule(const std::vector<Rule> &rules, int wday);
+const bool DEBUG = false;
+Schedule GetSchedule(const std::vector<Rule> &rules, int wday);
 
 int main()
 {
     std::vector<Rule> rules = GetRules();
-    int wday = GetCurTM()->tm_wday;
-    Rule Crule = GetCurRule(rules, wday);
+    int wday = GetCurTM()->tm_wday, now;
+    bool sync = false;
+    Schedule today = GetSchedule(rules, wday);
     std::thread Update(
         [&]()
         {
             while (1)
             {
+                now = GetCurTimeVal();
+                if (DEBUG)
+                    PDEBUGprint("Time Updated!\n");
                 int wd = GetCurTM()->tm_wday;
                 if (wd != wday)
                 {
                     if (DEBUG)
-                        Pprint("Day switched\n", P_BLUE);
+                        PDEBUGprint("Day switched!\n");
                     wday = wd;
-                    Crule = GetCurRule(rules, wday);
+                    today = GetSchedule(rules, wday);
                 }
-                std::this_thread::sleep_for(std::chrono::milliseconds(60000));
+                if (!sync)
+                {
+                    if (DEBUG)
+                        PDEBUGprint("Syncing......\n");
+                    int ss = GetCurTM()->tm_sec;
+                    if (DEBUG)
+                        PDEBUGprint("Sleeping......\n");
+                    std::this_thread::sleep_for(std::chrono::seconds(60 - ss));
+                    if (DEBUG)
+                        PDEBUGprint("Sync Finished!\n");
+                    now = GetCurTimeVal();
+                    if (DEBUG)
+                        PDEBUGprint("Time Updated!\n");
+                    sync = true;
+                }
+                std::this_thread::sleep_for(std::chrono::seconds(60));
             }
         });
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    int now = GetCurTimeVal();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     while (1)
     {
-        for (auto &i : Crule.timeseg)
+        for (auto &i : today.tasklist)
         {
-            while (now >= i.st && now <= i.ed)
+            while (now >= i.timeseg.st && now <= i.timeseg.ed)
             {
-                for (auto &j : Crule.command)
-                    Run(j);
-                if (DEBUG)
-                    Pprint("Run command. TimeVal:" + std::to_string(now) + '\n', P_BLUE);
-                std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-                now = GetCurTimeVal();
+                for (auto &j : i.command)
+                {
+                    if (DEBUG)
+                        PDEBUGprint("Run command:" + j + '\n');
+                    else
+                        Run(j);
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(i.sleep));
             }
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(60000));
-        now = GetCurTimeVal();
+        std::this_thread::sleep_for(std::chrono::seconds(30));
     }
     return 0;
 }
 
-Rule GetCurRule(const std::vector<Rule> &rules, int wday)
+Schedule GetSchedule(const std::vector<Rule> &rules, int wday)
 {
+    std::vector<Rule> select;
+    Schedule res;
     for (auto i = 0; i < rules.size(); i++)
         for (auto &j : rules[i].date)
             if (j == wday)
             {
                 if (DEBUG)
-                    Pprint("rule:" + std::to_string(i) + '\n' + "wday:" + std::to_string(wday) + '\n', P_BLUE);
-                return rules[i];
+                    PDEBUGprint("rule:" + std::to_string(i) + '\n');
+                select.emplace_back(rules[i]);
             }
-    return rules[0];
+    for (auto &i : select)
+        for (auto &j : i.timeseg)
+            res.tasklist.emplace_back(Task{j, i.command, i.sleep});
+    std::sort(res.tasklist.begin(), res.tasklist.end(), [](Task A, Task B)
+              { return A.timeseg.st < B.timeseg.st; });
+    return res;
 }
