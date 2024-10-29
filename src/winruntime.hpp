2048834e44e7facd8c04b8b573b96fa6eeec59fe
TTTPPPPP <906483498@qq.com>
2024-10-29 17:31:56 +0800

    Add is_block parameter to winruntime.hpp Run

--- src/winruntime.hpp
+++ src/winruntime.hpp
@@ -1,24 +1,26 @@
 #ifndef WINRUNTIME_HPP
 #define WINRUNTIME_HPP
 
-#include <windows.h>
 #include <string>
+#include <windows.h>
 
-inline int Run(const std::string &command)
-{
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
-    if (!CreateProcess(NULL, (LPSTR)&command[0], NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi))
-        return -1;
+inline int Run(const std::string &command, bool is_block = true) {
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+  ZeroMemory(&si, sizeof(si));
+  si.cb = sizeof(si);
+  ZeroMemory(&pi, sizeof(pi));
+  if (!CreateProcess(NULL, (LPSTR)&command[0], NULL, NULL, FALSE,
+                     CREATE_NO_WINDOW, NULL, NULL, &si, &pi))
+    return -1;
+  DWORD ExitCode = 1;
+  if (is_block) {
     WaitForSingleObject(pi.hProcess, INFINITE);
-    DWORD ExitCode;
     GetExitCodeProcess(pi.hProcess, &ExitCode);
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-    return ExitCode;
+  }
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+  return ExitCode;
 }
 
 #endif
